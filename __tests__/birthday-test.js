f12c3b2c9b66f6ec2ec23c91e0008c627fed9123
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-26 10:08:18 -0700

    pass age in days test, using predefined day input

--- __tests__/birthday-test.js
+++ __tests__/birthday-test.js
@@ -2,7 +2,7 @@ import { User } from "../src/birthday";
 
 describe('User', () => {
   test('Should return user age in Earth days', () => {
-  const userBirthday = new User;
-  expect(userBirthday.ageinDays).toBe(" ")
+  const userAge = new User(2700);
+  expect(userAge.ageInDays).toBe(2700)
   });
 });
