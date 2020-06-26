3f44a0c821e2245bcaa721eb588c3df085a5cfec
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-06-26 09:31:53 -0700

    add test for conversion to earth years, fail

--- __tests__/planet-age-test.js
+++ __tests__/planet-age-test.js
@@ -1,9 +1,9 @@
 import { Planet } from '../src/planet-age.js';
 
 describe('Planet', () => {
-  test('Should return " " when incorrect input is entered' , () => {
+  test('Should return undefined when incorrect input is entered' , () => {
     const mercury = new Planet
-    expect (mercury.days).toBe(0);
+    expect (mercury.days).toBe(undefined);
   });
 
   test('Should return planet days property', () => {
@@ -11,5 +11,10 @@ describe('Planet', () => {
     mercury.getDays();
     expect(mercury.days).toBe(88);
   });
-
+  
+  test('should convert planet days to Earth years', () => {
+    const mercury = new Planet("mercury")
+    mercury.convertToEarthYears();
+    expect(mercury.earthYears).toBe(.24)
+  });
 });
--- src/planet-age.js
+++ src/planet-age.js
@@ -1,13 +1,18 @@
 export class Planet {
   constructor (planetName) {
     this.planetName = planetName;
-    this.days = 0;
+    this.days;
+    this.earthYears;
   }
 
   getDays() {
     if (this.planetName === "mercury") {
       this.days = 88
     }
+  }
+
+  convertToEarthYears(){
+
   }
 }
 
